80b955f0ea32c3e3c355b93e37e4204df558d537
Ale2301 <alejo.2001@live.com.ar>
2025-03-27 14:05:39 -0300

    feat: shift flow

---
+++ scripts/flow_manager.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_turn_flow()
+
+
+func start_turn_flow():
+	await player_action()
+	await player_animation()
+	await update_stats()
+	await enemy_turn()
+	await enemy_status()
+	await enemy_action()
+	await update_stats()
+	start_turn_flow()
+
+func player_status():
+	print("Player animation status are running")
+	await get_tree().create_timer(1.0).timeout
+
+func player_action():
+	print("Player did something")
+	await get_tree().create_timer(1.0).timeout
+
+func player_animation():
+	print("Player animation is running")
+	await get_tree().create_timer(1.0).timeout
+
+func update_stats():
+	print("Life/Mana is updating")
+	await get_tree().create_timer(1.0).timeout
+
+func enemy_turn():
+	print("Enemy turn starts")
+	await get_tree().create_timer(1.0).timeout
+
+func enemy_status():
+	print("Enemy animation status are running")
+	await get_tree().create_timer(1.0).timeout
+
+func enemy_action():
+	print("Enemy did something")
+	await get_tree().create_timer(1.0).timeout
